6cc60b973f7123fe11dbbf540373967be55f465d
daniela09876 <daniela060215@gmail.com>
2024-01-11 12:04:05 -0500

    added methods for routes and URL parameter handling in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hola Mundo!'
+  }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'Yo soy nuevo';
+  }
+
+  @Get('/ruta/')
+  hello() {
+    return 'con /sas/'
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param() params: any) {
+    return `product ${params.productId}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
+    return `product ${productId} and ${id}`;
   }
 }

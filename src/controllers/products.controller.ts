e58dbb54591387e4c3dfe65c21c014bc3b52429e
daniela09876 <daniela060215@gmail.com>
2024-01-12 10:36:26 -0500

    Added POST, PUT, and DELETE handlers in Controller with successful Postman tests

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Param,
+    Query,
+    Post,
+    Body,
+    Put,
+    Delete
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -9,16 +18,45 @@ export class ProductsController {
         @Query('offset') offset = 0,
         @Query('brand') brand: string
     ) {
-        return `products: limit=> ${limit} offset=> ${offset} brand=>${brand}`
+        return {
+            message: `products: limit=> ${limit} offset=> ${offset} brand=>${brand}`
+        }
     }
 
     @Get('filter')
     getProductFilter() {
-        return `Soy un filter`;
+        return {
+            message: `Soy un filter`
+        };
     }
 
     @Get(':productId')
     getProduct(@Param() params: any) {
-        return `product ${params.productId}`;
+        return {
+            message: `product ${params.productId}`
+        };
+    }
+
+    @Post()
+    create(@Body() payload: any) {
+        return {
+            message: 'accion de crear',
+            payload,
+        };
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() payload: any) {
+        return {
+            id,
+            payload
+        };
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: number) {
+        return {
+            id
+        };
     }
 }

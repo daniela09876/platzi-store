989d0a1c4d944a09041e2443b225410f9636b2b3
daniela09876 <daniela060215@gmail.com>
2024-01-15 15:50:11 -0500

    Error handling in ProductsService, adding NotFoundException and tested in Postman

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -12,10 +12,14 @@ import {
     Res,
 } from '@nestjs/common';
 
-import { Response, response } from 'express';
+import { Response } from 'express';
+
+import { ProductsService } from '../services/products.service'
 
 @Controller('products')
 export class ProductsController {
+
+    constructor(private productsService: ProductsService) { }
 
     @Get()
     getProducts(
@@ -23,9 +27,10 @@ export class ProductsController {
         @Query('offset') offset = 0,
         @Query('brand') brand: string
     ) {
-        return {
-            message: `products: limit=> ${limit} offset=> ${offset} brand=>${brand}`
-        }
+        // return {
+        //     message: `products: limit=> ${limit} offset=> ${offset} brand=>${brand}`
+        // }
+        return this.productsService.findAll();
     }
 
     @Get('filter')
@@ -37,32 +42,29 @@ export class ProductsController {
 
     @Get(':productId')
     @HttpCode(HttpStatus.ACCEPTED)
-    getOne(@Res() response: Response, @Param('productId') productId: string) {
-        response.status(200).send({
-            message: `product ${productId}`
-        });
+    getOne(@Param('productId') productId: string) {
+        // response.status(200).send({
+        //     message: `product ${productId}`
+        // });
+        return this.productsService.finOne(+productId);
     }
 
     @Post()
     create(@Body() payload: any) {
-        return {
-            message: 'accion de crear',
-            payload,
-        };
+        // return {
+        //     message: 'accion de crear',
+        //     payload,
+        // };
+        return this.productsService.create(payload);
     }
 
     @Put(':id')
     update(@Param('id') id: number, @Body() payload: any) {
-        return {
-            id,
-            payload
-        };
+        return this.productsService.update(+id, payload);
     }
 
     @Delete(':id')
     delete(@Param('id') id: number) {
-        return {
-            id
-        };
+        return this.productsService.remove(+id);
     }
 }

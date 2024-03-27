e806cfb3e3589eb855e806afe303b0231add2879
daniela09876 <70279267+daniela09876@users.noreply.github.com>
2024-03-27 17:21:07 +0000

    update

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,11 @@ module.exports = {
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.ts?$': 'babel-jest'
   },
+  transformIgnorePatterns: [
+    "/node_modules/(?!@nestjs/common).+\\.js$",
+ ],
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',

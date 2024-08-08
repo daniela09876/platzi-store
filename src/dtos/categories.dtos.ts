6a1dccc619ed3b0e44628b9e3564b8d1daae33ea
daniela09876 <70279267+daniela09876@users.noreply.github.com>
2024-08-08 18:05:38 +0000

    format

--- src/dtos/categories.dtos.ts
+++ src/dtos/categories.dtos.ts
@@ -1,10 +1,10 @@
-import { IsString, IsNotEmpty } from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
+import { IsString, IsNotEmpty } from "class-validator";
+import { PartialType } from "@nestjs/mapped-types";
 
 export class CreateCategoryDto {
-  @IsString()
-  @IsNotEmpty()
-  readonly name: string;
+	@IsString()
+	@IsNotEmpty()
+	readonly name: string;
 }
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
